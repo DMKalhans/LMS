0195876b2565dae344cdb2cf1e0bd1f55e8938f8
shreyas1016 <111569029+Shreyassm1@users.noreply.github.com>
2025-09-07 13:07:21 +0530

    same-site error

--- Server/utils/generateToken.js
+++ Server/utils/generateToken.js
@@ -1,22 +1,28 @@
-import jwt from "jsonwebtoken"
-import dotenv from "dotenv"
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const generateToken = (res, user, message) => {
+  //console.log("User is received from the database inside generateToken as:", user);
 
-    //console.log("User is received from the database inside generateToken as:", user);
-
-    const token = jwt.sign({userId: user.id}, process.env.SECRET_KEY, {expiresIn:'1d'});
-    return res.status(200).cookie("token", token, {
-        httpOnly: true,
-        sameSite: "strict",
-        maxAge: 24*60*60*1000
-    }).json({
-        success:true,
-        message,
-        user
+  const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return res
+    .status(200)
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: true, // required for SameSite=None
+      sameSite: "None", // allow cross-site cookies
+      maxAge: 24 * 60 * 60 * 1000,
+      path: "/",
     })
-}
+    .json({
+      success: true,
+      message,
+      user,
+    });
+};
 
-export default generateToken
+export default generateToken;
